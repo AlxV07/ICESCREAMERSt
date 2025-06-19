478fcba477cfdad2cfa09499b3a9068a4de3c76d
AlxV07 <alexander.kai.chen@gmail.com>
2025-06-19 09:16:57 -0500

    documented script js

--- static/script.js
+++ static/script.js
@@ -1,4 +1,9 @@
+//  === Endpoint Connection Methods ===
+
 async function search() {
+  /*
+  Called by search button; sends search query to server, expects response in established search-query-response data format.
+  */
   const acronym = document.getElementById("acronym").value;
   const context = document.getElementById("context").value;
 
@@ -9,10 +14,28 @@ async function search() {
   });
 
   const results = await response.json();
-  const resultsDiv = document.getElementById("results");
 
+  handleSearchResponse(results);
+}
+
+
+async function define() {
+  /*
+
+  */
+}
+
+
+// === Frontend Util Methods ===
+
+
+async function handleSearchResponse(response) {
+  /*
+  Handles search-query-response from endpoint
+  response: search-query-response in established data format
+  */
+  const resultsDiv = document.getElementById("results");
   resultsDiv.innerHTML = results.length
     ? results.map(r => `<p><strong>${r.expansion}</strong>: ${r.description}</p>`).join("")
     : "<p>No results found.</p>";
 }
-

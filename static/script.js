4d7c8ed0a06c4b015b120d64df2bdf025331ed16
amnaashraf0 <ashrafamna000@gmail.com>
2025-06-19 11:42:11 -0500

    edit script.js

--- static/script.js
+++ static/script.js
@@ -1,18 +1,56 @@
-async function search() {
-  const acronym = document.getElementById("acronym").value;
-  const context = document.getElementById("context").value;
+//  === Endpoint Connection Methods ===
 
-  const response = await fetch("/lookup", {
+async function search() {
+  /*
+  Called by search button; sends search query to server, expects response in established search-query-response data format.
+  */
+  const acronym = document.getElementById("acronym").value;
+  const tags = document.getElementById("tags").value;
+
+  const response = await fetch("/search", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ acronym, context })
+    body: JSON.stringify({ acronym, tags })
   });
-
   const results = await response.json();
-  const resultsDiv = document.getElementById("results");
-
-  resultsDiv.innerHTML = results.length
-    ? results.map(r => `<p><strong>${r.expansion}</strong>: ${r.description}</p>`).join("")
-    : "<p>No results found.</p>";
+  console.log(results);
+  await handleSearchResponse(results);
 }
 
+
+async function define() {
+  const acronym = document.getElementById("defineAcronym").value;
+  const term = document.getElementById("defineTerm").value;
+  const definition = document.getElementById("defineDefinition").value;
+  const tags = document.getElementById("defineTags").value;
+  const misc = document.getElementById("defineMisc").value;
+
+  const response = await fetch("/define", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ acronym, term, definition, tags, misc })
+  });
+  const results = await response.json();
+  console.log(results);
+  await handleDefineResponse(results);
+}
+
+
+// === Frontend Util Methods ===
+
+
+async function handleSearchResponse(response) {
+  /*
+  Handles search-query-response from endpoint
+  response: search-query-response in established data format
+  */
+  const resultsDiv = document.getElementById("results");
+  console.log(response);
+  resultsDiv.innerHTML = JSON.stringify(response);
+  // TODO: Temporary; just displaying stringified display
+  // change to setting innerHTML to be styled response
+}
+
+async function handleDefineResponse(response) {
+  // TODO: Implement
+}
